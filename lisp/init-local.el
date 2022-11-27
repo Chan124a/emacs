126b271448ae9eec99d14923022d8959ecdf48a0
cpd_lianxiang <120633269@qq.com>
2022-11-27 16:54:08 +0800

    add c ide develop

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -24,6 +24,15 @@
 ;; (add-hook 'org-mode-hook 'yas-minor-mode) ;;只有在org文件下才打开yas功能
 ;; ;;(yas-global-mode 1) ;;启动自动打开yas功能
 
+(add-to-list 'load-path "~/emacs.d/lisp/c_ide")
+(require 'setup-general)
+;; (if (version< emacs-version "24.4")
+;;     (require 'setup-ivy-counsel)
+;;   (require 'setup-helm)
+;;   (require 'setup-helm-gtags))
+;; (require 'setup-ggtags)
+(require 'setup-cedet)
+(require 'setup-editing)
 
 
 (provide 'init-local)
