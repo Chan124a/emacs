23cc552a3c575dd3ce89f5fd77983013c5dc057a
cpd_lianxiang <120633269@qq.com>
2023-04-06 22:26:30 +0800

    20230406

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -12,28 +12,33 @@
   :custom
   (org-download-method 'directory)
   (org-download-image-dir "e:/org/images")
+  (org-download-heading-lvl 0)
   (org-download-screenshot-method "e:/IrfanView/i_view64.exe /capture=4 /convert=\"%s\"")
   :bind
   ("C-S-y" . org-download-screenshot)
   :config
   (require 'org-download))
 
-;; ;;启用yasnippet
-;; (require 'yasnippet)
-;; (yas-reload-all)
-;; (add-hook 'org-mode-hook 'yas-minor-mode) ;;只有在org文件下才打开yas功能
-;; ;;(yas-global-mode 1) ;;启动自动打开yas功能
+;;启用yasnippet
+(require 'yasnippet)
+(yas-reload-all)
+(add-hook 'org-mode-hook 'yas-minor-mode) ;;只有在org文件下才打开yas功能
+;;(yas-global-mode 1) ;;启动自动打开yas功能
 
-(add-to-list 'load-path "~/emacs.d/lisp/c_ide")
-(require 'setup-general)
-;; (if (version< emacs-version "24.4")
-;;     (require 'setup-ivy-counsel)
-;;   (require 'setup-helm)
-;;   (require 'setup-helm-gtags))
-;; (require 'setup-ggtags)
-(require 'setup-cedet)
-(require 'setup-editing)
+;;参考教程http://tuhdo.github.io/c-ide.html，设置c++ IDE环境
+;; (add-to-list 'load-path "~/emacs.d/lisp/c_ide")
+;; (require 'setup-general)
+;; ;; (if (version< emacs-version "24.4")
+;; ;;     (require 'setup-ivy-counsel)
+;; ;;   (require 'setup-helm)
+;; ;;   (require 'setup-helm-gtags))
+;; ;; (require 'setup-ggtags)
+;; (require 'setup-cedet)
+;; (require 'setup-editing)
 
+;;启用lsp
+;; (require 'lsp-mode)
+;; (add-hook 'XXX-mode-hook #'lsp)
 
 (provide 'init-local)
 ;;; init-locales.el ends here
