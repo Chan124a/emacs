d84196c87f95e9741365b8831030ec13fdb92ab2
cpd_lianxiang <120633269@qq.com>
2023-04-13 22:12:43 +0800

    lx_增加HOME的设置

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -40,5 +40,8 @@
 ;; (require 'lsp-mode)
 ;; (add-hook 'XXX-mode-hook #'lsp)
 
+;; 设置这个是为了让magit的ssh服务能够正常运行
+(setenv "HOME" "C:\\Users\\123")
+
 (provide 'init-local)
 ;;; init-locales.el ends here
